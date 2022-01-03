a5a231126bfd1b5b250bc2f992513e0ed8a9d083
Maxence Machu <maxence@akarah.com>
2022-01-03 08:29:32 +0100

    add(router)

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,16 +1,9 @@
-<script>
-import Menu from './components/Menu.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Menu
-  }
-}
-</script>
-
 <template>
-  <Menu/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <style lang="scss">
@@ -20,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
