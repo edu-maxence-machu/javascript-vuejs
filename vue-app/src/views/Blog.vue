993db22aef27f794d636a55a6a43a8fe0de0ee12
Maxence Machu <maxence@akarah.com>
2022-01-04 08:34:27 +0100

    update(fetch) & components

---
+++ vue-app/src/views/Blog.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="blog__article">
+        <h1>Hello blog: {{ title }} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+        data() { 
+            return {title: "Titre de l'article" }
+        },
+        beforeCreate() { console.log('Je ne suis pas encore  créé') },
+        created() { console.log('Je suis créé !') },
+        beforeMount() { console.log('Je vais bientôt être monté sur le DOM!') },
+        mounted() { console.log('Je suis monté sur le DOM!') },
+        beforeUnmount() { console.log('Je suis sur le point de disparaître du DOM !') },
+        unmounted() { console.log('Je suis supprimé') }
+}
+</script>
+
+<style>
+
+</style>
--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h2>Articles</h2>
+
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
   </div>
 </template>
 
@@ -13,6 +19,26 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("https://my-json-server.typicode.com/typicode/demo/posts");
+        this.posts = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  } ,       
+  data() { 
+    return {
+      posts: [] 
+    }
+  },
+  mounted() { 
+        this.getData();
+   },
+  beforeUnmount() { console.log('Je suis sur le point de disparaître du DOM !') },
+  unmounted() { console.log('Je suis supprimé') }
 }
 </script>
